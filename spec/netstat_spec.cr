b96d9182bd793aa87abfe4295487750468cdd3ca
William Harris <wharris@upscalews.com>
2022-11-04 16:36:53 -0400

    setup for testing more things

--- spec/netstat_spec.cr
+++ spec/netstat_spec.cr
@@ -12,27 +12,30 @@ describe "netstat" do
     end
     ClusterTools.install
   end
-
-  it "'helm' should install cnf with two services on the cluster that connect to the same database" do
+  
+  describe "cnf with two services on the cluster that connect to the same database" do
     # sample-cnfs/ndn-multi-db-connections-fail
     release_name = "wordpress"
     helm_chart_directory = "sample-cnfs/ndn-multi-db-connections-fail/wordpress"
-    install_success = true
-    helm_install(release_name, helm_chart_directory)
+
+    it "should install" do
+      helm_install(release_name, helm_chart_directory)
+    end
   ensure
     resp = Helm.uninstall(release_name)
     Log.info { resp }
     resp[:status].exit_status == 0
   end
   
+  describe "cnf with two services on the cluster that connect to the same database" do
   # sample-cnfs/sample-statefulset-cnf
-  it "'helm' should install cnf with no database used by two microservices" do
-    # sample-cnfs/ndn-multi-db-connections-fail
     Helm.helm_repo_add("bitnami", "https://charts.bitnami.com/bitnami")
     release_name = "test"
     helm_chart = "bitnami/wordpress"
-    install_success = true
-    helm_install(release_name, helm_chart, nil, "--set mariadb.primary.persistence.enabled=false --set persistence.enabled=false")
+
+    it "should install" do
+      helm_install(release_name, helm_chart, nil, "--set mariadb.primary.persistence.enabled=false --set persistence.enabled=false")
+    end
   ensure
     resp = Helm.uninstall(release_name)
     Log.info { resp }
